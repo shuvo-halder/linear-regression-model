806f283ecdc4159682d7fb8b8c17a5beb05f40ca
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-26 21:41:42 +0600

    matplotlib init

---
+++ desprective-Matplotlib.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = {
+    'Student_ID': np.arange(1, 6),
+    'Name': ['John', 'Emily', 'Michael', 'Sophia', 'William'],
+    'Age': [20, 21, 19, 22, 20],
+    'Grade': [3.8, 3.9, 3.5, 4.0, 3.7],
+    'Major': ['Computer Science', 'Biology', 'Mathematics', 'Engineering', 'Psychology']
+}
+
+student_df = pd.DataFrame(data)
+
+
+# using matplot 
+
+plt.hist(student_df['Age'], bins=10)
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.title('Age')
+plt.show()
