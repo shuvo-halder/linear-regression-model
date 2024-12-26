f5accdf3fbf7533d5d7af5bdd062474d6d1be71b
SHUVO HALDER <samshuvo00@gmail.com>
2024-12-26 21:06:01 +0600

    DataFrame

---
+++ eda DataFrame.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+data = {
+    'Student_ID': np.arange(1, 6),
+    'Name': ['John', 'Emily', 'Michael', 'Sophia', 'William'],
+    'Age': [20, 21, 19, 22, 20],
+    'Grade': [3.8, 3.9, 3.5, 4.0, 3.7],
+    'Major': ['Computer Science', 'Biology', 'Mathematics', 'Engineering', 'Psychology']
+}
+
+student_df = pd.DataFrame(data)
+
+print(student_df)
