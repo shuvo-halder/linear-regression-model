3eef93077d9b45e244c04d7bafb2bc1b2470768b
SHUVO HALDER <samshuvo00@gmail.com>
2025-01-07 23:51:15 +0600

    data pre processing start

---
+++ Data Cleaning and Processing and Transformation/data_preprocessing.py
@@ -0,0 +1,16 @@
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,RobustScaler,Normalizer,QuantileTransformer,PowerTransformer,LabelEncoder,OneHotEncoder,OrdinalEncoder,KBinsDiscretizer
+
+import pandas as pd
+import numpy as np
+import os
+
+# Load the data
+file_name = "Data Cleaning and Processing and Transformation/data.csv"
+script_dir = os.getcwd()
+data = pd.read_csv(os.path.normcase(os.path.join(script_dir, file_name)))
+
+
+scaler = StandardScaler()
+
+df_scaled = scaler.fit_transform(data)
+print(df_scaled)
